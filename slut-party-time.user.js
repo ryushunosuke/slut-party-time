38d0654e298ac477e81a63426eaf8c20bdae27e7
Ryuunosuke <ryuu@waifu.club>
2019-03-28 08:50:36 +0300

    it now semi-works

--- slut-party-time.user.js
+++ slut-party-time.user.js
@@ -12,6 +12,8 @@ function sleep(ms) {
 }
 
 async function sluts(){
+  var userbackgroundpng = document.getElementById('user-background-style'); //uses the meguca custom background option
+  var userbackgroundwebm = document.getElementById('user-background');
   var thread = document.getElementById('thread-container');
   var last = thread.lastElementChild;
   while (last.nextSibling == null){
@@ -21,16 +23,20 @@ async function sluts(){
       lastpostHTML = last.innerHTML.slice(last.innerHTML.search('a href="/assets/'),last.innerHTML.length);
       lastpostHTML = lastpostHTML.slice(8,lastpostHTML.search('" download='));
       if(lastpostHTML.search('.webm')!=-1){
-      userbackground = document.getElementById('user-background');
-      userbackground.firstChild.replaceWith(`<div id="user-background"><video autoplay="" loop=""><source src="$LINK" type="video/webm"></video></div>`.replace("$LINK", lastpostHTML));
-      userbackground.firstChild.load();
+        if (userbackgroundwebm.firstChild === null){
+          userbackgroundwebm.appendChild(document.createElement("video"));
+        }
+      userbackgroundwebm.firstChild.src= lastpostHTML;
+        userbackgroundwebm.firstChild.autoplay = true;
+        userbackgroundwebm.firstChild.loop = true;
+      userbackgroundwebm.firstChild.load();
       }
       else{ //Means last post has media inside of it and it is not a webm
-        var userbackground = document.getElementById('user-background-style'); //uses the meguca custom background option
-        if (userbackground.childNodes !== undefined || userbackground.childElementNodes != 0){ 
-          userbackground.removeChild(userbackground.firstChild); //Removes the webm container which seems to have a priority over images/gifs
+
+        if (userbackgroundwebm.lastChild !== null){ 
+          userbackgroundwebm.removeChild(userbackgroundwebm.firstChild); //Removes the webm container which seems to have a priority over images/gifs
         }
-        userbackground.firstChild.replaceWith(`#user-background {background: url($LINK) no-repeat fixed center; background-size:cover;background-size: cover;}`.replace("$LINK",lastpostHTML));
+        userbackgroundpng.firstChild.replaceWith(`#user-background {background: url($LINK) no-repeat fixed center; background-size:cover;background-size: cover;}`.replace("$LINK",lastpostHTML));
 
       }
     }
